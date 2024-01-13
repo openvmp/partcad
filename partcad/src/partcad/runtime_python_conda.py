45e45a1338c16ad86f59783a6634d28a5824f279
Roman Kuzmenko <openvmp@proton.me>
2024-01-13 13:26:20 -0800

    More fixes for windows

--- partcad/src/partcad/runtime_python_conda.py
+++ partcad/src/partcad/runtime_python_conda.py
@@ -17,42 +17,44 @@ class CondaPythonRuntime(runtime_python.PythonRuntime):
     def __init__(self, ctx, version=None):
         super().__init__(ctx, "conda", version)
 
-        if not self.initialized:
-            self.conda_path = shutil.which("conda")
-            if self.conda_path is None:
-                raise Exception(
-                    "ERROR: PartCAD is configured to use conda, but conda is missing"
-                )
-
-            try:
-                os.makedirs(self.path)
-                subprocess.run(
-                    [
-                        self.conda_path,
-                        "create",
-                        "-y",
-                        "-p",
-                        self.path,
-                        "python=%s" % version,
-                    ]
-                )
-                subprocess.run(["conda", "install", "-y", "-p", self.path, "pip"])
-                self.initialized = True
-            except Exception as e:
-                shutil.rmtree(self.path)
-                raise e
+        self.conda_path = shutil.which("conda")
 
     def run(self, cmd, stdin=""):
-        return super().run(
-            [
-                self.conda_path,
-                "run",
-                "--no-capture-output",
-                "-p",
-                self.path,
-                # "python",  # Trust conda to set the link correctly
-                "python%s" % self.version,  # This doesn't work on Windows
-            ]
-            + cmd,
-            stdin,
-        )
+        with self.lock:
+            if not self.initialized:
+                if self.conda_path is None:
+                    raise Exception(
+                        "ERROR: PartCAD is configured to use conda, but conda is missing"
+                    )
+
+                try:
+                    os.makedirs(self.path)
+                    subprocess.run(
+                        [
+                            self.conda_path,
+                            "create",
+                            "-y",
+                            "-p",
+                            self.path,
+                            "python=%s" % self.version,
+                        ]
+                    )
+                    subprocess.run(["conda", "install", "-y", "-p", self.path, "pip"])
+                    self.initialized = True
+                except Exception as e:
+                    shutil.rmtree(self.path)
+                    raise e
+
+            return super().run(
+                [
+                    self.conda_path,
+                    "run",
+                    "--no-capture-output",
+                    "-p",
+                    self.path,
+                    "python",
+                    # "python%s" % self.version,  # This doesn't work on Windows
+                ]
+                + cmd,
+                stdin,
+            )
