a5fb3a9700e24b7044515da11b8d39c6f84b5d58
PartCAD GitHub Action <openvmp@proton.me>
2024-01-14 07:35:49 +0000

    Version updated from 0.3.6 to 0.3.7

--- partcad-cli/src/partcad_cli/__init__.py
+++ partcad-cli/src/partcad_cli/__init__.py
@@ -4,4 +4,4 @@ __all__ = [
     "main_cli",
 ]
 
-__version__: str = "0.3.6"
+__version__: str = "0.3.7"
--- partcad/src/partcad/__init__.py
+++ partcad/src/partcad/__init__.py
@@ -39,4 +39,4 @@ __all__ = [
     "main_cli",
 ]
 
-__version__: str = "0.3.6"
+__version__: str = "0.3.7"
