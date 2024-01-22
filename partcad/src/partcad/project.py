b73408f581bf6d818a479b045b4d8806c0c688d9
Roman Kuzmenko <openvmp@proton.me>
2024-01-21 19:27:23 -0800

    Fixed a mistake in the part creation code

--- partcad/src/partcad/project.py
+++ partcad/src/partcad/project.py
@@ -233,8 +233,8 @@ class Project(project_config.Configuration):
         found = False
         for elem in config:
             if elem == section:
-                section = config[section]
-                section[name] = obj
+                config_section = config[section]
+                config_section[name] = obj
                 found = True
                 break  # no need to iterate further
         if not found:
