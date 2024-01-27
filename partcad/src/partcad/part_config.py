a39438c6b1c105460f48d0f0c6b48ed2eaf8e2b4
OpenVMP <openvmp@proton.me>
2024-01-27 14:11:24 -0800

    Minor fix for boolean params (#102)

--- partcad/src/partcad/part_config.py
+++ partcad/src/partcad/part_config.py
@@ -38,6 +38,11 @@ class PartConfiguration(ShapeConfiguration):
                         "type": "float",
                         "default": param_value,
                     }
+                elif isinstance(param_value, bool):
+                    config["parameters"][param_name] = {
+                        "type": "bool",
+                        "default": param_value,
+                    }
                 # All params are float unless another type is explicitly speciifed
                 elif isinstance(param_value, dict) and not "type" in param_value:
                     param_value["type"] = "float"
