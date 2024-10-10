31965749010cfb2e539b54bddacf812be901b0a4
Roman Kuzmenko <openvmp@proton.me>
2024-10-09 22:46:48 -0700

    fix dependencies

--- partcad/src/partcad/sketch_factory_build123d.py
+++ partcad/src/partcad/sketch_factory_build123d.py
@@ -99,11 +99,11 @@ class SketchFactoryBuild123d(SketchFactoryPython):
             request_serialized = base64.b64encode(picklestring).decode()
 
             await self.runtime.ensure("ocp-tessellate")
+            await self.runtime.ensure("cadquery")
             await self.runtime.ensure("numpy==1.24.1")
             await self.runtime.ensure("numpy-quaternion==2023.0.4")
             await self.runtime.ensure("nptyping==1.24.1")
             await self.runtime.ensure("typing_extensions>=4.6.0,<5")
-            await self.runtime.ensure("cadquery")
             await self.runtime.ensure("build123d")
             cwd = self.project.config_dir
             if self.cwd is not None:
